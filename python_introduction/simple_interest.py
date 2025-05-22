3719b1a6f185587e3116ce5333bd16d138c1e455
jonasnikitha <jonasnikitha@gmail.com>
2025-05-22 20:20:04 +0200

    Added new file that calculates simple interest in the python_introduction folder

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+# Assigning values
+principal = 1000
+rate = 0.05
+time =3
+
+#Calculate simple_interest
+simple_interest = float(principal*rate*time)
+
+#Printing results
+print('The simple interest is:', simple_interest)
+
