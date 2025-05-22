9faf802a8a64c8aad920e6e894f314c95982df1a
jonasnikitha <jonasnikitha@gmail.com>
2025-05-22 20:47:47 +0200

    Fixed the area formula on my  reactangle_area.py file

--- python_introduction/rectangle_area.py
+++ python_introduction/rectangle_area.py
@@ -1,9 +1,9 @@
 #Assigning values
 length = 10
-width =5
+width = 5
 
 #Calculating area of a rectangle
-area = length*width
+area = length * width
 
 #Printing results
 print("The area of the rectangle is:", area)
