3de951465a40010816d08b20e17a66bb3e9e0cd7
jonasnikitha <jonasnikitha@gmail.com>
2025-05-29 21:08:42 +0200

    Added a pttern drawing file using nested loops

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,8 @@
+#User enetr number for pattern drawing
+size = int(input("Enter the size of the pattern: "))
+row= 0
+while row < size:
+    for i in range(size):
+        print("*", end="")
+    print()
+    row += 1
