10c22cfcf79e4ed5b2eeed0fd1cc8c3faa331e68
jonasnikitha <jonasnikitha@gmail.com>
2025-05-29 20:13:27 +0200

    Added a multplication file

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,4 @@
+#User to input number for a multiplication table
+number = int(input("Enter a number to see its multiplication table: "))
+for i in range(1, 11):
+    print(f"{number} x {i} = {number* i}")
